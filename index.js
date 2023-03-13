5018f29325715a2f8dd4704b5193963984827015
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-03-13 19:38:23 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const firstNames = [];
+function writeCards(firstNames, eventName) {
+    const message = [];
+    for (let i = 0; i < firstNames.length; i++){
+        message.push(`Thank you, ${firstNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    console.log(message);
+    return message;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(){
+    let num = 10
+    while (num >= 0){
+        console.log(num);
+        num--;
+    }
+}
