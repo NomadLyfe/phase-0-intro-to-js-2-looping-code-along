32d71e35251f08f50f63f66fd9b7a4b45829318c
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-06 14:44:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const firstNames = [];
-function writeCards(firstNames, eventName) {
-    const message = [];
-    for (let i = 0; i < firstNames.length; i++){
-        message.push(`Thank you, ${firstNames[i]}, for the wonderful ${eventName} gift!`);
+function writeCards(names, adjective) {
+    const thankYouMessages = [];
+    for (let name in names) {
+        thankYouMessages[name] = `Thank you, ${names[name]}, for the wonderful ${adjective} gift!`;
     }
-    console.log(message);
-    return message;
+    return thankYouMessages;
 }
-writeCards(["Charlie", "Samip", "Ali"], "birthday");
-
-function countDown(){
-    let num = 10
-    while (num >= 0){
-        console.log(num);
-        num--;
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
     }
 }
